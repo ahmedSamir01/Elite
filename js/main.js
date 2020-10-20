f6974ee9bba237204fad085b60e23fd5aa4e52fc
ahmed samir <asproducer80@gmail.com>
2020-10-20 18:26:52 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,97 @@
+/*global $, alert, console*/
+
+$(function () {
+
+    "use strict";
+
+/*    
+    let winH = $(window).height(),
+        upperH = $(".upper-bar").innerHeight(),
+        navH = $("nav").innerHeight();
+    */
+    
+    $(window).on("load resize", function() {
+        
+        $(".slider, .slider .carousel-item").height($(window).height() - ($(".upper-bar").innerHeight() + $("nav").innerHeight()) + 1);
+        
+    });
+    
+    
+    $(".navbar li").on("click", function (b) {
+
+        b.preventDefault();
+
+        console.log($(this).data('scroll'));
+        $('html, body').animate({
+
+            scrollTop: $('#' + $(this).data('scroll')).offset().top - 35
+
+        }, 2000);
+
+        $(".navbar li").removeClass("active");
+        $(this).addClass("active");
+
+    });
+
+    $(".navbar li a").on("click", function (b) {
+
+        b.preventDefault();
+
+        $('html, body').animate({
+
+            scrollTop: $($(this).data('scroll')).offset().top - 35
+
+        }, 2000);
+
+        $(".navbar a").removeClass("active");
+        $(this).addClass("active");
+
+    });
+    
+    var sc = $('.scrollToTop');
+      
+    $(window).on("scroll", function () {
+        
+        if ($(window).scrollTop() >= 1000) {
+
+            if (sc.is(':hidden')) {
+
+                sc.fadeIn();
+
+            }
+
+        } else {
+
+            sc.fadeOut();
+
+        }    
+        
+    });
+    
+    sc.on("click", function (e) {
+            
+        e.preventDefault();
+            
+        $("html, body").animate({
+               
+            scrollTop: 0
+               
+        }, 2000);
+            
+    });
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/plugins.js
@@ -0,0 +1,24 @@
+// trigger nice scroll
+$(window).on("load", function() {
+    
+    setTimeout(function () {
+        $("html").niceScroll({
+
+            cursorborder: 0,
+            cursorborderradius: '25px',
+            cursorwidth: '7px',
+            cursorcolor: '#08526d',
+            cursorfixedheight: 120,
+            zindex:'99999999'
+
+
+        });
+    }, 500);
+});    
+
+    
+    
+    
+    
+    
+    
