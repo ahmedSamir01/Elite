1b44d9608b08f88795f8b209a0feeace3d4055eb
ahmed samir <asproducer80@gmail.com>
2020-10-21 03:41:26 +0200

    edit11

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ $(function () {
         $(".navbar-collapse").removeClass("show");
         $("body, html").animate({
            
-            scrollTop: $("#" + $(this).data('scroll')).offset().top
+            scrollTop: $("#" + $(this).children().data('scroll')).offset().top
             
         }, 1000);
 
